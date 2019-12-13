46ff8b3a7fa380835704b112c9972c4b996ef9d1
pbcpraveen <pbcpraveen@gmail.com>
2019-12-13 15:11:16 +0530

    particle

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '{{base_path}}/assets/particles.json', function() {
+particlesJS.load('particles-js', 'https://pbcpraveen.github.io/assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 })
